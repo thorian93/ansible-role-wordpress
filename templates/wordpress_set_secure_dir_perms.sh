cc6f121d3581398aff6fb42fc95d17821b178ed8
Thorian93 <info@thorian93.de>
2020-12-13 19:27:56 +0100

    Add content.

---
+++ templates/wordpress_set_secure_dir_perms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wppath='{{ wordpress_web_dir}}'
+htuser='{{ apache2_http_user }}'
+htgroup='{{ apache2_http_user }}'
+rootuser='root'
+
+
+printf "chmod Files and Directories\n"
+find ${wppath}/ -type f -print0 | xargs -0 chmod 0644
+find ${wppath}/ -type d -print0 | xargs -0 chmod 0755
+
+printf "chown Directories\n"
+chown -R ${htuser}:${htgroup} ${wppath}/
